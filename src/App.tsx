ad5d1f7d513ed70d340bd78b6cc2138db90d1d75
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-24 07:05:25 +0200

    fix debug 2

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
 	return (
 		<div className="App">
 			<pre className="debug">
-				baseUrl = {baseUrl} (v2)
+				baseUrl = {baseUrl}
 			</pre>
 			<h1>Language Tandem Group</h1>
 			{currentUser.username !== 'anonymousUser' && (
